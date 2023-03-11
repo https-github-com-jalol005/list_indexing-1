ffdfe5cdb706e43f940da7016b3111eadf0cfe91
Mirjalol <jalolubaydullayev575@gmail.com>
2023-03-11 10:33:22 +0500

    list10

--- bin/list_index10.dart
+++ bin/list_index10.dart
@@ -6,16 +6,17 @@
     Returns:
         list: return answer
 */
-List func(List list_num){
-    List a;
-    if(list_num.first<list_num.last){
-        a=[list_num.last];
-    }else{
-        a=[list_num.first];
-    }
-    return a;
+List func(List list_num) {
+  List a;
+  if (list_num.first < list_num.last) {
+    a = [list_num.last];
+  } else {
+    a = [list_num.first];
+  }
+  return a;
 }
+
 void main() {
-    print(func([3,6,4]));
+  print(func([6, 6, 6]));
 }
 
