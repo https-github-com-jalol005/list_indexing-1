5e00dd9234ef962d6a20ae20561f0dbc86f7ce54
Mirjalol <jalolubaydullayev575@gmail.com>
2023-03-11 10:31:56 +0500

    list09

--- bin/list_index09.dart
+++ bin/list_index09.dart
@@ -6,4 +6,15 @@
     Returns:
         bool: return answer
 */
-void main() {}
+List func(List list_num){
+    List a;
+    if(list_num.first<list_num.last){
+        a=[list_num.last];
+    }else{
+        a=[list_num.first];
+    }
+    return a;
+}
+void main() {
+    print(func([3,6,4]));
+}
